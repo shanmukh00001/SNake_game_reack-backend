8d0156df224920e4b8b1416050a88d4735b42b10
shanmukh00001 <shanmukhanagasaineduri@gmail.com>
2026-04-13 19:56:54 +0530

    fix cors

--- server/server.js
+++ server/server.js
@@ -8,12 +8,7 @@ import scoreRoutes from "./routes/scoreRoutes.js";
 dotenv.config();//Keeps sensitive/config data out of code
 const app = express();
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    process.env.FRONTEND_URL
-  ].filter(Boolean),
-  credentials: true
+  origin: "*"
 }));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
